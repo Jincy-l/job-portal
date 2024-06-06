a12e34f3dfcf171de1a8b32ea9bbced0fbf53a59
jincy l <jincy@gmail.com>
2024-06-06 09:55:37 +0530

    update

--- job/job/settings.py
+++ job/job/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobport',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'job.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'jobportal',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+
+        'PORT':'3306',
     }
 }
 
--- job/jobport/views.py
+++ job/jobport/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def index(request):
+    return redirect(request,"index.html"),
+def error(request):
+    return redirect(request,"error.html"),
+def about(request):
+    return redirect(request,"about.html"),
+def category(request):
+    return redirect(request,"category.html"),
+def contact(request):
+    return redirect(request,"contact.html"),
+def jobdeatail(request):
+    return redirect(request,"job-detail.html"),
+def joblist(request):
+    return redirect(request,"job-list.html"),
+def testimonial(request):
+    return redirect(request,"testimonial.html"),
