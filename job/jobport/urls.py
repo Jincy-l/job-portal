5b9c75c6e743b82affe325cb19b27bf725061c2c
jincy l <jincy@gmail.com>
2024-06-06 13:25:31 +0530

    login

--- job/jobport/urls.py
+++ job/jobport/urls.py
@@ -4,7 +4,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(" ",views.index,name='index'),
-    path("index",views.index,name='index'),
+    path("",views.index,name='index'),
+    # path("index",views.index,name='index'),
+    path("about",views.about,name='about'),
+    path("category",views.category,name='category'),
+    path("error",views.error,name='error'),
+    path("jobdetail",views.jobdetail,name='jobdetail'),
+    path("joblist",views.joblist,name='joblist'),
+    path("testimonial",views.testimonial,name='testimonial'),
+    
     
 ]
--- job/jobport/views.py
+++ job/jobport/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def index(request):
-    return redirect(request,"index.html"),
+    return render(request,"index.html")
 def error(request):
-    return redirect(request,"error.html"),
+    return render(request,"error.html")
 def about(request):
-    return redirect(request,"about.html"),
+    return render(request,"about.html")
 def category(request):
-    return redirect(request,"category.html"),
+    return render(request,"category.html")
 def contact(request):
-    return redirect(request,"contact.html"),
-def jobdeatail(request):
-    return redirect(request,"job-detail.html"),
+    return render(request,"contact.html")
+def jobdetail(request):
+    return render(request,"jobdetail.html")
 def joblist(request):
-    return redirect(request,"job-list.html"),
+    return render(request,"joblist.html")
 def testimonial(request):
-    return redirect(request,"testimonial.html"),
+    return render(request,"testimonial.html")
